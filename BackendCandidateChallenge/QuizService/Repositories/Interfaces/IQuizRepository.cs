d2c95c8bd565722c9466274693d7a9a0aaa47dfe
dzihanovicelvedin <dzihanovicelvedin@gmail.com>
2022-10-25 21:52:12 +0200

    Add QuizRepository and move logic from controllers

---
+++ BackendCandidateChallenge/QuizService/Repositories/Interfaces/IQuizRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuizService.Model;
+using QuizService.Model.Domain;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QuizService.Repositories.Interfaces
+{
+    public interface IQuizRepository
+    {
+        public IEnumerable<QuizResponseModel> Get();
+        public QuizResponseModel Get(int id);
+
+    }
+}
